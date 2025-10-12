e1b4582dfc72fe81d71d6eaad5cfdbbceddf12a0
Affanraza Shaikh <116919245+Affanraza2002@users.noreply.github.com>
2025-10-12 06:34:10 -0700

    Update LandingPage.jsx

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -83,7 +83,7 @@ function LandingPage() {
         <div>
           <section className="flex items-center justify-center -mt-36">
             <img
-              src={__  }
+              src=""
               alt="Intro image Image"
               className="w-[80vw] rounded-lg border-1 border-teal-300 box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);  "
             />
